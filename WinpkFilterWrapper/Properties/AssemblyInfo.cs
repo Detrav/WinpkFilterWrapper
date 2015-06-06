53f152e4815c3ba5d02d78c72b93359157a7dab4
Detrav <witalyezep@gmail.com>
2015-06-06 12:15:14 +0300

    Релиз 1.0

--- WinpkFilterWrapper/Properties/AssemblyInfo.cs
+++ WinpkFilterWrapper/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("WinpkFilterWrapper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("HOME")]
 [assembly: AssemblyProduct("WinpkFilterWrapper")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
